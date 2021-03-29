6d6fee1dd3f07dd10ea021ff1fd3e4e58479b57b
Laurentlcs <lucas.laurent27@gmail.com>
2021-03-29 14:12:06 +0200

    create layout and add nuxt-property-decorator lib

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,34 @@
 <template>
-  <div>
+  <div class="parent">
+    <div class="header">
+      <!-- Header Component -->
+      <h3>HEADER</h3>
+    </div>
+    <div class="teamBanner">
+      <!-- TeamBanner Component -->
+      <h3>TEAM-BANNER</h3>
+    </div>
     <Nuxt />
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+.parent {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  max-height: 100vh;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+.header {
+  grid-area: 1 / 1 / 2 / 6;
+  background-color: red;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.teamBanner {
+  grid-area: 2 / 1 / 3 / 6;
+  background-color: blue;
 }
 </style>
